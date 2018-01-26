740db033f841c7b64b4b3f36014558e31aca7ef1
w_sen <w_sen@flashbox.de>
2018-01-26 19:28:49 +0100

    chore(local): 206 Navigating Back & To Next Page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -22,14 +22,23 @@ class Checkout extends Component {
     // this.setState({ingredients: ingredients});
   }
 
-  componentCancelHandler = () => {
-
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
   }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
 
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
     );
   }
