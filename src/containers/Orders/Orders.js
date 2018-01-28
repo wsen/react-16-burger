902b463d17aad2a208cb0c16b27e35ef781e79d2
w_sen <w_sen@flashbox.de>
2018-01-28 21:09:48 +0100

    chore(local): 212 Implementing Nav Links, Fetching Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get('./orders.json')
+      .then(res => {
+        const fetchedOrders = []
+        for (let key in res.dat) {
+          fetchedOrders.push({
+            ...res.data[key],
+          });
+        }
+        this.setState({loading: false, orders: fetchedOrders})
+      })
+      .catch(err => {
+        this.setState({loading: false});
+      });
+  }
+
   render () {
     return (
-      <Order />
-      <Order />
+      <div>
+        <Order />
+        <Order />
+      </div>
     );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
