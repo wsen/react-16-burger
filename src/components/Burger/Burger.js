92f9bd1a6e90d0cac2573e5117dc2932bdf01756
w_sen <w_sen@flashbox.de>
2018-01-28 22:27:55 +0100

    chore(local): 213 Outputting Orders

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,14 +7,14 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = ( props ) => {
     ///console.log(props);
     let transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-          return <BurgerIngredient key={igKey + i} type={igKey} />
-        });
-    })
-    .reduce((arr, el) => {
-      return arr.concat(el)
-    }, []);
+      .map(igKey => {
+          return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+          } );
+      })
+      .reduce((arr, el) => {
+        return arr.concat(el)
+      }, []);
 
     if (transformedIngredients.length === 0) {
       transformedIngredients = <p>Please start adding Zutaten!</p>
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -14,9 +14,10 @@ class Orders extends Component {
     axios.get('./orders.json')
       .then(res => {
         const fetchedOrders = []
-        for (let key in res.dat) {
+        for (let key in res.data) {
           fetchedOrders.push({
             ...res.data[key],
+            id: key
           });
         }
         this.setState({loading: false, orders: fetchedOrders})
@@ -29,8 +30,12 @@ class Orders extends Component {
   render () {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order key={order.id}
+          ingredients={order.ingredients}
+          price={order.price}
+           />
+        ))}
       </div>
     );
   }
