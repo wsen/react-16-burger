e6b6764d5b814c58680a25388a11322b1a897fc3
w_sen <w_sen@flashbox.de>
2018-01-31 22:39:43 +0100

    chore(local): 223 Handling Form Submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -65,13 +65,18 @@ class ContactData extends Component {
     loading: false
   }
 
-  orderHandler = (event) => {
+  orderHandler = ( event ) => {
     event.preventDefault();
     console.log(this.props.ingredients);
     this.setState({loading: true});
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     }
 
     axios.post('/orders.json', order)
@@ -107,7 +112,7 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
               key={formElement.id}
@@ -118,7 +123,7 @@ class ContactData extends Component {
           />
         ))}
 
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        <Button btnType="Success">ORDER</Button>
        </form>
     );
     if (this.state.loading) {
