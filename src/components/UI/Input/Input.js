bd8930b731ad8913b4a3cf95c15be5ad95bb44fa
w_sen <w_sen@flashbox.de>
2018-01-31 22:13:45 +0100

    chore(local): 222 Handling User Input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,25 +4,27 @@ import classes from './Input.css';
 
 const input = (props) => {
   let inputElement = null;
-console.log(props);
   switch ( props.elementType ) {
     case ( 'input' ):
       inputElement = <input
       className={classes.InputElement}
       {...props.elementConfig}
-      value={props.value}/>;
+      value={props.value}
+      onChange={props.changed} />;
       break;
     case ( 'textarea' ):
       inputElement = <textarea
       className={classes.InputElement}
       {...props.elementConfig}
-      value={props.value}/>;
+      value={props.value}
+      onChange={props.changed} />;
       break;
     case ( 'select' ):
       inputElement = (
         <select
           className={classes.InputElement}
-          value={props.value}>
+          value={props.value}
+          onChange={props.changed} >
           {props.elementConfig.options.map(option => (
           <option key={option.value} value={option.value}>
             {option.displayValue}
@@ -35,7 +37,8 @@ console.log(props);
       inputElement = <input
       className={classes.InputElement}
       {...props.elementConfig}
-      value={props.value}/>;
+      value={props.value}
+      onChange={props.changed} />;
   }
 
   return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,6 +85,18 @@ class ContactData extends Component {
       });
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  }
+
   render () {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
