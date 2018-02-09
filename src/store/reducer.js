142ac5102979aa53ba7ba5b5557228abe7871db0
w_sen <w_sen@flashbox.de>
2018-02-09 17:29:34 +0100

    chore(local): 257 Finishing the Reducer for Ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,12 @@
 import * as actionTypes from './actions';
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4
 };
 
@@ -13,12 +18,23 @@ const reducer = (state = initialState, action) => {
             }
             return {
                 ...state,
-                ingredients: state.ingredients.concat(newIngredient)
-            }
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
         case actionTypes.REMOVE_INGREDIENT:
-        
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default:
+            return state;     
     }
-        
+    return state;
 };
 
 export default reducer;
