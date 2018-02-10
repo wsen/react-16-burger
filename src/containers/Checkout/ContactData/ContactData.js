ab0c8b3df7926e08ef5ca24f323811439e96c336
w_sen <w_sen@flashbox.de>
2018-02-10 14:37:51 +0100

    fix(local): form control email regExpr

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -51,7 +51,8 @@ class ContactData extends Component {
             validation: {
                 required: true,
                 minLength: 5,
-                maxLength: 5
+                maxLength: 5,
+                isNumeric: true
             },
             valid: false,
             touched: false,
@@ -81,7 +82,8 @@ class ContactData extends Component {
             value: '',
             validation: {
                 required: true,
-                minLength: 3
+                minLength: 3,
+                isEmail: true
             },
             valid: false,
             touched: false,
@@ -142,6 +144,16 @@ class ContactData extends Component {
 
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
     }
 
     return isValid;
